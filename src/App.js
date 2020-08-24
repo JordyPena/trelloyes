e2ed2871331fb4c9e699a48b02079a38068f246d
Jordy Pena <jordypenamontes@gmail.com>
2020-08-24 17:58:01 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List';
+import './app.css'
 
-function App() {
+
+
+function App(props) {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+
+        </h1>
       </header>
-    </div>
-  );
+
+      <div className="App-list">
+  {props.store.lists.map(list => {
+    let cards = list.cardIds.map(item => props.store.allCards[item])
+    return <List header={list.header} cards={cards}/>
+  })}
+
+      </div>
+    
+    </main>
+  )
+
 }
 
 export default App;
